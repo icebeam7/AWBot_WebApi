822aa65662e423e588d1f7277ddd6a5658c141ae
icebeam7 <luis.beltran@itcelaya.edu.mx>
2020-07-04 15:45:51 +0200

    Initial Version

---
+++ AWBot_WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AWBot_WebApi.Models;
+using AWBot_WebApi.Context;
+
+namespace AWBot_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly AdventureWorksLTContext _context;
+
+        public CustomersController(AdventureWorksLTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers/email
+        [HttpGet("{email}")]
+        public ActionResult<CustomerShort> GetCustomer(string email)
+        {
+            var customer = _context.Customer.FirstOrDefault(x => x.EmailAddress == email);
+
+            if (customer != null)
+            {
+                return new CustomerShort()
+                {
+                    EmailAddress = customer.EmailAddress,
+                    CompanyName = customer.CompanyName,
+                    CustomerName = $"{customer.Title} {customer.FirstName} {customer.LastName} {customer.Suffix}"
+                };
+            }
+
+            return default(CustomerShort);
+        }
+    }
+}
---
+++ AWBot_WebApi/Models/CustomerShort.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AWBot_WebApi.Models
+{
+    public class CustomerShort
+    {
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
